3400412c495d171a2b96ca10d850edfb954d0966
Numan Özdemir <84520977+Black-Nemo@users.noreply.github.com>
2025-12-02 22:05:50 +0300

    main.py kaldırıldı

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-import resources.PdfAIEntegrations.ai_promt_manager as promt_manager
-import resources.CardSystem.card_manager as card_manager
-
-pdf_path = "C:/Users/nmnzd/Desktop/Ders/yazılıkmanalizi/hafta1_v3.pdf"
-result = promt_manager.send_pdf_path_get_card_data(pdf_path)
-
-cards = card_manager.get_card_list(result.text)
-
-card_manager.save_cards(cards,"cikti1.json")
